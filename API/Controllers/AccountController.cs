e767328502d07c32c01edeeba3b137877cd7478e
Valdir Marcheuski Junior <valdir.mjunior@hotmail.com>
2024-08-10 10:27:59 -0300

    End of section 4.
    Added multiple controllers, services, authorization and authentication with JwtToken.

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+            {
+                return BadRequest("Username is taken");
+            }
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser()
+            {
+                UserName = registerDto.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var userDto = new UserDto()
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+
+            return Ok(userDto);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            if (!computedHash.SequenceEqual(user.PasswordHash))
+                return Unauthorized("Invalid password");
+
+            var userDto = new UserDto()
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+
+            return Ok(userDto);
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,10 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,6 +14,9 @@ app.UseCors(x =>
      .AllowAnyMethod()
      .WithOrigins("http://localhost:4200", "https://localhost:4200")
      );
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
